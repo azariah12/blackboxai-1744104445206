9e1f017f537f62155da0392274127ea759e8214a
azariah12 <110028740+azariah12@users.noreply.github.com>
2025-04-09 07:54:50 +0000

    Add main.js 1744185288070

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true
+    },
+    icon: path.join(__dirname, 'icon.ico')
+  })
+
+  win.loadFile('index.html')
+  
+  // Remove default menu
+  win.removeMenu()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
